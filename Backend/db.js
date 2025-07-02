1f9f4c120339ecf27d3b7c0abeb4952778b526d7
Shivam <sshivamgupta833@gmail.com>
2025-07-02 20:41:45 +0530

    connected to the database

--- Backend/db.js
+++ Backend/db.js
@@ -2,12 +2,33 @@ const mysql =  require('mysql2');
 require('dotenv').config({path: '../.env'})
 const mysql_password = process.env.mysql_password
 
-const pool = mysql.createPool({
+const pool = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
     password: mysql_password,
     database: 'MeetingRoomBookingApplication'
-}).promise()
+})
 
-const result = await pool.query('SELECT * FROM MeetingRoomBookingApplication')
-console.log(result)
+console.log('Mysql connected successfully')
+
+
+async function createUsersTable(){
+
+    try{
+
+        
+        await pool.execute(`
+            CREATE TABLE users(
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(100) NOT NULL,
+                password VARCHAR(100) NOT NULL,
+                email VARCHAR(100) NOT NULL UNIQUE
+                )
+                `)
+                }
+                catch(err){
+                    console.log(`error while creating a table ${err}`)
+                }
+    }
+
+    createUsersTable();
