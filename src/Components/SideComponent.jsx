83381f45fc139ef94dc36cbb8268ae8e2b5bdd6f
Shivam <sshivamgupta833@gmail.com>
2025-07-10 13:42:09 +0530

    Button, Input, Sidecomponent for Auth page

---
+++ src/Components/SideComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function SideComponent() {
+  return (
+    <div className="h-screen  p-4 flex  items-center justify-around flex-col text-white bg-[radial-gradient(122.11%_84.18%_at_50%_100%,_#EDD9FC_0%,_#994FDE_35.62%,_#52299B_63.24%,_#000_100%)] w-full">
+      <h1 className="text-7xl font-bold">MeetSpace </h1>
+
+      <p className="text-4xl text-center">
+        Smart scheduling made simple, For teams that Value time.
+      </p>
+    </div>
+  );
+}
